051646fa12b1424e91dd70ca3520d44e73929580
JonasNon <officialjonasn@gmail.com>
2024-08-20 21:11:24 -0500

    added ListArticles component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect} from 'react';
+import ListArticles from './ListArticles';
 
 function App() {
+
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    fetch('http://hn.algolia.com/api/v1/items/3')
+        .then(response => {
+          return response.json()
+       }).then(arrayOfArticles => setArticles(arrayOfArticles))
+       .catch(error => console.error('Error fetching users:', error));
+  }, []);
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          First Change
-        </a>
+        <ListArticles
+        articles = {articles}
+
+        />
       </header>
     </div>
   );
